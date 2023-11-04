e56a852063a9a2c392bf913ba5433ebd884f317f
yashdy <dhyaniyash234@gmail.com>
2023-11-05 05:06:45 +0530

    add content

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -30,30 +30,20 @@ const Hero = () => {
                     <svg class="w-2.5 h-2.5 mr-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
                         <path d="M17 11h-2.722L8 17.278a5.512 5.512 0 0 1-.9.722H17a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1ZM6 0H1a1 1 0 0 0-1 1v13.5a3.5 3.5 0 1 0 7 0V1a1 1 0 0 0-1-1ZM3.5 15.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM16.132 4.9 12.6 1.368a1 1 0 0 0-1.414 0L9 3.55v9.9l7.132-7.132a1 1 0 0 0 0-1.418Z"/>
                     </svg>
-                    Design
+                    Security
                 </a>
-                <h2 class="text-white dark:text-white text-3xl font-extrabold mb-2">Start with Flowbite Design System</h2>
-                <p class="text-lg font-normal text-white dark:text-gray-400 mb-4">Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers.</p>
-                <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">Read more
-                    <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                </svg>
-                </a>
+                <h2 class="text-white dark:text-white text-3xl font-extrabold mb-2">Secure platform with NFT Authentication</h2>
+                <p class="text-lg font-normal text-white dark:text-gray-400 mb-4">Each piece of content like movie, show and series is assigned with unique NFT. This NFT is linked to the user's account when they purchase or subscribe to the content.</p>
             </div>
             <div class="bg-gray-950 dark:bg-gray-800 dark:border-gray-700 rounded-lg p-8 md:p-12">
                 <a href="#" class="bg-purple-100 text-purple-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-purple-400 mb-2">
                     <svg class="w-2.5 h-2.5 mr-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 4 1 8l4 4m10-8 4 4-4 4M11 1 9 15"/>
                     </svg>
-                    Code
+                    Revenue
                 </a>
-                <h2 class="text-white dark:text-white text-3xl font-extrabold mb-2">Best react libraries around the web</h2>
-                <p class="text-lg font-normal text-white dark:text-gray-400 mb-4">Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers.</p>
-                <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">Read more
-                    <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                    </svg>
-                </a>
+                <h2 class="text-white dark:text-white text-3xl font-extrabold mb-2">Maximize the platform's revenue.</h2>
+                <p class="text-lg font-normal text-white dark:text-gray-400 mb-4">Content creators and streaming platform can maximize their revenue by preventing unauthorized access to their content.</p>
             </div>
         </div>
     </div>
